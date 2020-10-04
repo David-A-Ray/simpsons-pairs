67227a015226866c3a6702fd5ac46fb2463fd2e7
phil-griffith <philgriffith@outlook.com>
2020-10-04 17:27:44 +0100

    Set delayed animations on Welcome screen with Javascript

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+// **************************** Main Menu  *********************************************
+
+setTimeout(function() {
+  $('.opening-screen')[0].classList.add('animate__zoomOutUp')
+  $('.menu-items').css('visibility','visible');
+  $('.menu-items')[0].classList.add('animate__animated', 'animate__backInUp', 'animate__delay-1s')
+
+},3000);
